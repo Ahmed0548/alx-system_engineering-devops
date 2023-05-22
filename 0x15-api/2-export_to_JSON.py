58546f215554a9f0c8a85c0d07356c52670a5307
Ahmed0548 <ahmedalkasape@gmail.com>
2023-05-22 10:56:23 -0700

    Task 2

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,59 +1,41 @@
 #!/usr/bin/python3
-"""script that fetches info about a given employee using an api
-and exports it in json format
 """
+Script that, using this REST API, for a given employee ID, returns
+information about his/her TODO list progress
+and export data in the JSON format.
+"""
+
 import json
 import requests
-import sys
+from sys import argv
 
-
-base_url = 'https://jsonplaceholder.typicode.com'
 
 if __name__ == "__main__":
 
-    user_id = sys.argv[1]
+    sessionReq = requests.Session()
 
-    # get user info e.g https://jsonplaceholder.typicode.com/users/1/
-    user_url = '{}/users?id={}'.format(base_url, user_id)
-    # print("user url is: {}".format(user_url))
+    idEmp = argv[1]
+    idURL = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(idEmp)
+    nameURL = 'https://jsonplaceholder.typicode.com/users/{}'.format(idEmp)
 
-    # get info from api
-    response = requests.get(user_url)
-    # pull data from api
-    data = response.text
-    # parse the data into JSON format
-    data = json.loads(data)
-    # extract user data, in this case, username of employee
-    user_name = data[0].get('username')
-    # print("id is: {}".format(user_id))
-    # print("username is: {}".format(user_name))
+    employee = sessionReq.get(idURL)
+    employeeName = sessionReq.get(nameURL)
 
-    # get user info about todo tasks
-    # e.g https://jsonplaceholder.typicode.com/users/1/todos
-    tasks_url = '{}/todos?userId={}'.format(base_url, user_id)
-    # print("tasks url is: {}".format(tasks_url))
+    json_req = employee.json()
+    usr = employeeName.json()['username']
 
-    # get info from api
-    response = requests.get(tasks_url)
-    # pull data from api
-    tasks = response.text
-    # parse the data into JSON format
-    tasks = json.loads(tasks)
-    # print("JSOON LOADS IS: {}".format(tasks))
+    totalTasks = []
+    updateUser = {}
 
-    dict_key = str(user_id)
-    # print("dict_key: {}".format(dict_key))
+    for all_Emp in json_req:
+        totalTasks.append(
+            {
+                "task": all_Emp.get('title'),
+                "completed": all_Emp.get('completed'),
+                "username": usr,
+            })
+    updateUser[idEmp] = totalTasks
 
-    # build the json
-    builder = {dict_key: []}
-    for task in tasks:
-        json_data = {
-            "task": task['title'],  # or use get method
-            "completed": task['completed'],
-            "username": user_name
-        }
-        # append dictionary key to the dictionary
-        builder[dict_key].append(json_data)
-    json_encoded_data = json.dumps(builder)
-    with open('{}.json'.format(user_id), 'w', encoding='UTF8') as myFile:
-        myFile.write(json_encoded_data)
+    file_Json = idEmp + ".json"
+    with open(file_Json, 'w') as f:
+        json.dump(updateUser, f)
